1decabd91faaa5c34f6d4392e270732dd49233f6
Cory <chanson5000@gmail.com>
2018-03-14 21:26:36 -0500

    Switched to use site-wide CSS style instead of admin.

--- admin/index.php
+++ admin/index.php
@@ -6,14 +6,14 @@ include(SHARED_PATH . '/staff_header.php');
 ?>
 
 <h2 class="center">Administration</h2>
-<div class="admin-index">
+<div class="index-title">
     <table>
         <tr>
             <th colspan="2"><h3>What would you like to administer?</h3></th>
         </tr>
         <tr>
-            <td><a href="<?php echo url_for('admin/red-wine/index.php'); ?>">Red Wine</a></td>
-            <td><a href="<?php echo url_for('admin/white-wine/index.php'); ?>">White Wine</a></td>
+            <td><a href="<?php echo url_for('admin/red-wine/index.php'); ?>">Red</a></td>
+            <td><a href="<?php echo url_for('admin/white-wine/index.php'); ?>">White</a></td>
         </tr>
         <tr>
             <td colspan="2"><a href="<?php echo url_for('admin/user/index.php'); ?>">Users</a></td>
