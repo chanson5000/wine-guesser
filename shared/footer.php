30c5f788941b7984739ef63319cb5d049e43d4a6
Cory <chanson5000@gmail.com>
2018-04-25 17:03:25 -0500

    No more div!

--- shared/footer.php
+++ shared/footer.php
@@ -1,7 +1,6 @@
 <footer>
     <div class="what-is-bottom-link"><a href="<?php echo url_for('/about.php'); ?>">What is Wine Guesser?</a></div>
     &copy; <?php echo date('Y'); ?> <a href="http://www.coryhanson.us" target="_blank">Cory Hanson</a>
-
     <?php if (is_logged_in()) { ?>
         <div class="btm-return-link">
             Welcome, <?php echo isset($_SESSION['username']) ? $_SESSION['username'] : 'Unknown'; ?>. <a
@@ -9,9 +8,7 @@
             <a href="<?php echo url_for('admin/index.php'); ?>">Go to the administration page.</a>
         </div>
     <?php } ?>
-
 </footer>
-</div>
 </body>
 </html>
 
