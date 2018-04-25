a54ea9ab48a21f02f58f70346893f500c11111b6
Cory <chanson5000@gmail.com>
2018-04-25 16:35:06 -0500

    Removed some tags I didn't need.

--- shared/footer.php
+++ shared/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p><a href="<?php echo url_for('/about.php'); ?>">What is Wine Guesser?</a></p>
+    <div class="what-is-bottom-link"><a href="<?php echo url_for('/about.php'); ?>">What is Wine Guesser?</a></div>
     &copy; <?php echo date('Y'); ?> <a href="http://www.coryhanson.us" target="_blank">Cory Hanson</a>
 
     <?php if (is_logged_in()) { ?>
@@ -11,6 +11,7 @@
     <?php } ?>
 
 </footer>
+</div>
 </body>
 </html>
 
