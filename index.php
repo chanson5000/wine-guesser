0f1ee2e12d76db71ac2e8cbfc3b4ccfe40247e71
Cory <chanson5000@gmail.com>
2018-04-25 17:03:55 -0500

    Div'ed it up a little better.

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ $page_index = '1';
 
 include(SHARED_PATH . '/header.php');
 ?>
-
-<div class="index-title">
-    <table>
-        <tr>
-            <th colspan="2"><h2>Guess a wine.</h2></th>
-        </tr>
-        <tr>
-            <td><a id="wine-buttons" href="red-wine.php">Red</a></td>
-            <td><a id="wine-buttons" href="white-wine.php">White</a></td>
-        </tr>
-    </table>
+<div class="center">
+    <div class="index-title">
+        <table>
+            <tr>
+                <th colspan="2"><h2>Guess a wine.</h2></th>
+            </tr>
+            <tr>
+                <td><a id="wine-buttons" href="red-wine.php">Red</a></td>
+                <td><a id="wine-buttons" href="white-wine.php">White</a></td>
+            </tr>
+        </table>
+    </div>
 </div>
-
 <?php
 include(SHARED_PATH . '/footer.php');
 ?>
